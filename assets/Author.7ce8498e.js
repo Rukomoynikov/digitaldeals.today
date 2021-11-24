800df584d2e73a153d905c336460a543ba02eb15
Rukomoynikov <>
2021-11-24 15:56:39 +0000

    Deploying to gh-pages from @ Rukomoynikov/digitaldeals.today@cecde233014207c3efd8b2066c4a2eed2e4bf533 🚀

---
+++ assets/Author.7ce8498e.js
@@ -0,0 +1,5 @@
+import{c as n,a as m,b as p,r as i,d as A}from"./vendor.2bce37cd.js";n(import.meta.url,{modules:[],hydratedComponents:[],hoisted:[]});const h=m(import.meta.url,"https://astro.build");p(async(t,r,a)=>{const s=t.createAstro(h,r,a),{name:o,href:e}=s.props,d=[{props:{"data-astro-id":"BL6UQIAR"},children:".author.astro-BL6UQIAR{margin-bottom:0.75rem;}"}];for(const c of d)t.styles.add(c);return i`
+<div class="author astro-BL6UQIAR">
+  <p class="astro-BL6UQIAR"><a${A(e,"href")} class="astro-BL6UQIAR">${o}</a></p>
+</div>
+`});
